4b4e2f904978c1f445d8f7bd259892b7087b2735
zakyarifudin <zakyarifudin9@gmail.com>
2018-10-24 20:19:27 +0700

    tambah controller comment

---
+++ app/Controllers/Http/CommentController.js
@@ -0,0 +1,120 @@
+'use strict'
+
+const Database  = use('Database');
+const User      = use('App/Models/User');
+const Post      = use('App/Models/Post');
+const Comment   = use('App/Models/Comment');
+const { validate, validateAll } = use('Validator');
+
+class CommentController {
+
+    async byPost ({response, auth, params:{post_id}}) {
+        const comments = await Comment.query()
+                         .where('post_id', post_id)
+                         .with('post')
+                         .fetch();
+        return response.json({
+            status  : 'success',
+            result  : comments
+        })
+    }
+
+    async byUser ({response, auth, params:{user_id}}) {
+        const comments = await Comment.query()
+                         .with('post')
+                         .where('user_id', user_id)
+                         .fetch();
+        return response.json({
+            status  : 'success',
+            result  : comments
+        })
+    }
+
+    async store({ request, response, auth , params:{post_id}}) {
+        const rules = {
+            comment     : 'required'
+        }
+
+        const messages = {
+            'comment.required' : 'Komentar harus diisi' 
+        }
+        
+        const validation = await validateAll(request.all(), rules, messages)
+
+        if(validation.fails()){
+            return validation.messages()
+        }
+        
+        const user = await User.find(auth.user.id);
+
+        const comment = await user.comments()
+                    .create({
+                        post_id     : post_id,
+                        comment     : request.body.comment
+                    });
+        
+        if(!comment){
+            return response.json({
+                status  : 'fail',
+                message : 'Post is not available',
+            })
+        }
+        
+        return response.json({
+            status  : 'success',
+            message : 'Comment successfully created',
+            result  : comment
+        })
+    }
+
+    async update({ request, response, params:{id}, auth }) {
+
+        const comment = await Comment.query()
+                    .where('user_id', auth.user.id)
+                    .where('id',id)
+                    .first();
+
+        if(comment == null){
+            return response.json({
+                status : 'fail',
+                message: 'Failed to Update Comment not Found',
+            })
+        }
+
+        comment.comment = request.body.comment;
+        await comment.save();
+        
+        return response.json({
+            status  : 'success',
+            message : 'Comment successfully updated',
+            result  : comment
+            
+        })
+
+        
+    }
+
+    async destroy({ response, params: {id}, auth }) {
+        const comment = await Comment.query()
+                    .where('user_id', auth.user.id)
+                    .where('id',id)
+                    .first();
+
+        if(comment == null){
+            return response.json({
+                status  : 'fail',
+                message : 'Failed to Comment not Found',
+            })
+        }
+
+        await comment.delete();
+        
+        return response.json({
+            status    : 'success',
+            message   : 'Comment successfully deleted',
+            result    : id       
+        })
+    }
+}
+
+module.exports = CommentController
--- start/routes.js
+++ start/routes.js
@@ -25,10 +25,23 @@ Route.group(() => {
 .prefix('api/user');
 
 Route.group(() => {
+  //post 
   Route.get('' , 'PostController.index').middleware(['auth']);
+  Route.get('/own' , 'PostController.own').middleware(['auth']);
   Route.post('', 'PostController.store').middleware(['auth']);
   Route.get('/:id', 'PostController.show').middleware(['auth']);
   Route.put('/:id', 'PostController.update').middleware(['auth']);
   Route.delete('/:id', 'PostController.destroy').middleware(['auth']);
+
+  //comment
+  Route.post('/:post_id/comment', 'CommentController.store').middleware(['auth']);
+  Route.put('/comment/:id', 'CommentController.update').middleware(['auth']);
+  Route.delete('/comment/:id', 'CommentController.destroy').middleware(['auth']);
 })
 .prefix('api/post');
+
+Route.group(() => {
+  Route.get('/by-post/:post_id' , 'CommentController.byPost').middleware(['auth']);
+  Route.get('/by-user/:user_id' , 'CommentController.byUser').middleware(['auth']); 
+})
+.prefix('api/comment');
